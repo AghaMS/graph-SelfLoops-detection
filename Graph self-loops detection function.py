a3d498de3cac65fc62563f383264c327fee6d1c2
Mohammed Alagha <48173989+AghaMS@users.noreply.github.com>
2020-09-07 23:41:46 +0300

    Update Graph self-loops detection function.py

--- Graph self-loops detection function.py
+++ Graph self-loops detection function.py
@@ -3,13 +3,13 @@
 
 # A simple function to detect self-loops in graphs
 
-# In[1]:
-
-
 def Self_Loops(G):
     '''
     Detect self loops in a network
     '''
+    bold_start = '\033[1m'
+    bold_end   = '\033[0m'
+    
     nodes_in_selfloops = []
     for u, v in G.edges():
         if u == v:
@@ -17,10 +17,4 @@ def Self_Loops(G):
 
     if len(nodes_in_selfloops) > 0:
         print(bold_start,"Warning: Graph has self-loops. The nodes with self loops are:",bold_end, nodes_in_selfloops)
-
-
-# In[ ]:
-
-
-
-
+ 
